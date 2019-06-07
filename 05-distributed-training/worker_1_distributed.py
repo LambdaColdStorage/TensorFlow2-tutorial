70cd08bce4c69db2a5333546dc954f53e2753a7e
chuanli11 <cl.chuanli@gmail.com>
2019-06-07 12:50:02 -0700

    Working on distributed training

---
+++ 05-distributed-training/worker_1_distributed.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import os
+import json
+
+from tensorflow import keras
+import resnet
+
+os.environ["TF_CONFIG"] = json.dumps({
+    'cluster': {
+        'worker': ["10.1.10.244:12345", "10.1.10.250:12345"]
+    },
+    'task': {'type': 'worker', 'index': 1}
+})
+
+strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+
+
+NUM_GPUS = 2
+BS_PER_GPU = 128
+NUM_EPOCHS = 60
+
+HEIGHT = 32
+WIDTH = 32
+NUM_CHANNELS = 3
+NUM_CLASSES = 10
+NUM_TRAIN_SAMPLES = 50000
+
+BASE_LEARNING_RATE = 0.1
+LR_SCHEDULE = [(0.1, 30), (0.01, 45)]
+
+
+def normalize(x, y):
+  x = tf.image.per_image_standardization(x)
+  return x, y
+
+
+def augmentation(x, y):
+    x = tf.image.resize_with_crop_or_pad(
+        x, HEIGHT + 8, WIDTH + 8)
+    x = tf.image.random_crop(x, [HEIGHT, WIDTH, NUM_CHANNELS])
+    x = tf.image.random_flip_left_right(x)
+    return x, y	
+
+
+def schedule(epoch):
+  initial_learning_rate = BASE_LEARNING_RATE * BS_PER_GPU / 128
+  learning_rate = initial_learning_rate
+  for mult, start_epoch in LR_SCHEDULE:
+    if epoch >= start_epoch:
+      learning_rate = initial_learning_rate * mult
+    else:
+      break
+  tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
+  return learning_rate
+
+
+(x,y), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+train_dataset = tf.data.Dataset.from_tensor_slices((x,y))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+tf.random.set_seed(22)
+train_dataset = train_dataset.map(augmentation).map(normalize).shuffle(NUM_TRAIN_SAMPLES).batch(BS_PER_GPU * NUM_GPUS, drop_remainder=True)
+test_dataset = test_dataset.map(normalize).batch(BS_PER_GPU * NUM_GPUS, drop_remainder=True)
+
+
+input_shape = (HEIGHT, WIDTH, NUM_CHANNELS)
+img_input = tf.keras.layers.Input(shape=input_shape)
+opt = keras.optimizers.SGD(learning_rate=0.1, momentum=0.9)
+
+
+with strategy.scope():
+  model = resnet.resnet56(img_input=img_input, classes=NUM_CLASSES)
+  model.compile(
+            optimizer=opt,
+            loss='sparse_categorical_crossentropy',
+            metrics=['sparse_categorical_accuracy']) 
+model.fit(train_dataset,
+          epochs=NUM_EPOCHS)
